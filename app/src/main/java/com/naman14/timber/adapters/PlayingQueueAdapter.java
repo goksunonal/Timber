cda2b208ca4bd8092fee36d594a41e03c44ec5ea
goksunonal <goksunonal@gmail.com>
2017-10-27 16:11:15 +0300

    Clear Queue and Delete From Queue functions added.

--- app/src/main/java/com/naman14/timber/adapters/PlayingQueueAdapter.java
+++ app/src/main/java/com/naman14/timber/adapters/PlayingQueueAdapter.java
@@ -109,6 +109,9 @@ public class PlayingQueueAdapter extends RecyclerView.Adapter<PlayingQueueAdapte
                                 long[] deleteIds = {arraylist.get(position).id};
                                 TimberUtils.showDeleteDialog(mContext,arraylist.get(position).title, deleteIds, PlayingQueueAdapter.this,position);
                                 break;
+                            case R.id.popup_song_delete_from_queue:
+                                MusicPlayer.deleteFromQueue( -1);
+                                break;
                         }
                         return false;
                     }
